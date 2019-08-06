48a5f37628f7fb8cba2621edf94eb0f530fc0cd1
Syed Tariq <sydtrq7@gmail.com>
2019-08-06 21:34:26 +0530

    Works

--- app/src/main/java/com/example/quickksilent/MapsActivity.java
+++ app/src/main/java/com/example/quickksilent/MapsActivity.java
@@ -1,5 +1,38 @@
 package com.example.quickksilent;
+import android.*;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
 
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,6 +44,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -24,6 +59,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 /**
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,17 +68,45 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 */
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng arg0) {
+                mMap.clear();
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(arg0)
+                        .title("Set Location")
+                        .icon(BitmapDescriptorFactory
+                                .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                Double lati = (arg0.latitude);
+                Double loni = (arg0.longitude);
+                Toast.makeText(getApplicationContext(),""+lati,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),""+loni,Toast.LENGTH_SHORT).show();
+              //  String aLatPlace = lati.toString();
+              //  String aLongPlace = loni.toString();
+
+                //  Log.d("Latitude",""+lati);
+                //  Log.d("Longitude",""+loni);
+                //  Log.d("Latitude_String",aLatPlace);
+                //  Log.d("Longitude_String",aLongPlace);
+                // aftersettingMarker(lati,loni);
+
+            }
+        });
+
         if (mLocationPermissionsGranted) {
-            getDeviceLocation();
+            // getDeviceLocation();
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
@@ -49,29 +114,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return;
             }
             mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            //  LatLng sydney = new LatLng(17.4085452, 78.4475166);
+            //   googleMap.addMarker(new MarkerOptions().position(sydney)
+            //          .title("House"));
+            //  googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            //   mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
         }
     }
+
     private static final String TAG = "MapsActivity";
+
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
 
-    //widgets
-    private EditText mSearchText;
-    private ImageView mGps,mSearch;
-
     //vars
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
+        /**
+         * Code for geofencing - Basically alerts when you breach a paticuar location
+         * A lot is pending
+         * I'm listing out the functionality for later testing
+         * Basically, once the marker is set,the add button gets enabled and then after that
+         * the latitude and the longitude will be passed to a after marking function - whose job will be to call
+         * a fucking database helper class to save the values into a database.
+         * Database fields = id,lat,longi,enabled(boolean)
+         * Find a way to utilise geofencing,the code below to actually trigger location based alerts and set the phone
+         * silent or normal
+         *
+         */
+     //   geofencingClient = LocationServices.getGeofencingClient(this);
         getLocationPermission();
     }
 
@@ -111,6 +191,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
+
     private void initMap(){
         Log.d(TAG, "initMap: initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -141,20 +222,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
         Log.d(TAG, "onRequestPermissionsResult: called.");
         mLocationPermissionsGranted = false;
 
@@ -173,13 +242,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     //initialize our map
                     initMap();
                 }
-                }
             }
         }
-
-
-    private void hideSoftKeyboard(){
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
+    public void aftersettingMarker(double lati,double longi){
+
+
+
+    }
 }
